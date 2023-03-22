84132c7c15e14a906fdaa4046768bcb378f8b900
omar <omar.graouna@gmail.com>
2023-03-22 23:24:29 +0100

    Initial commit

---
+++ src/main/java/com/example/sspring1/Sspring1Application.java
@@ -0,0 +1,13 @@
+package com.example.sspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sspring1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sspring1Application.class, args);
+    }
+
+}
